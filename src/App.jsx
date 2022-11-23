ea8a684da690dd171a1d63786a87136dab964aa1
Francisco Arques <franarq05@hotmail.com>
2022-11-23 20:39:29 -0300

    subida inicial de archivos

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import './App.css';
+import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <NavBar/>
+        <ItemListContainer greeting="¡Futura App E-commerce!"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+const ItemListContainer = (props) =>  {
+    return (
+        <div className="flex place-content-center mt-6 text-2xl">
+            {props.greeting}
+        </div>
+    )
+}
+
+export default ItemListContainer
